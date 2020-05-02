fe4368f559bb4287b59212b0058cfb23f8de5a87
STERIN557 <sterin557@gmail.com>
2020-05-02 20:05:33 +0530

    added india live update api

--- index.js
+++ index.js
@@ -42,6 +42,100 @@ for(let i=0;i<data.Countries.length;i++){
 }
 
 // Send request
-request.send()
+request.send();
+
+
+
+
+
+$(".div1").click(()=>{
+ $(".informations").css("display","block");
+ $(".informations1").css("display","none");
+ 
+})
+
+$(".div2").click(()=>{
+  $(".informations1").css("display","flex");
+  $(".informations").css("display","none");
+  
+ })
+ 
+
+
+
+
+
+
+
+//   mnlhlhhkhklhlk
+
+var request1 = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request1.open('GET', 'https://api.covid19india.org/raw_data3.json', true)
+
+request1.onload = function(response) {
+    
+var data1  = JSON.parse(this.response)
+
+if (request1.status >= 200 && request1.status < 400) {
+    //  console.log(data.Countries[0].Country)
+    // console.log(data1.raw_data.lenght);
+
+    for(let i=0;i<data1.raw_data.length;i++)
+    {
+
+     
+     
+    $("main").append(` 
+
+
+    <div class="informations1">
+          
+    <p>Age : <span id="age"> ${data1.raw_data[i].agebracket || 'not available'}</span></p> 
+    <p>Current Status : <span id="current"> ${data1.raw_data[i].currentStatus || 'not available'}</span></p>
+    <p>Date Announced : <span id="date">${ data1.raw_data[i].dateannounced ||'not available'}</span></p>
+    <p>district :<span id="district">${ data1.raw_data[i].detecteddistrict || 'not available'}</span></p>
+    <p>State :<span id="state"> ${data1.raw_data[i].detectedState || 'not available'}</span></p>
+    <p>Gender :<span id="gender"> ${data1.raw_data[i].gender || 'not available'}</span></p>
+    <p>Info source : <span id="source"> ${data1.raw_data[i].source1  || 'not available'}</span></p>
+
+
+   
+</div>
+
+
+
+`);
+    
+
+
+      
+    }
+ 
+} 
+  
+  else {
+    console.log('error')
+  }
+
+
+  // Begin accessing JSON data here
+}
+
+// Send request
+request1.send();
+
+
+
+
+
   
 })
+
+
+
+
+
+
+
